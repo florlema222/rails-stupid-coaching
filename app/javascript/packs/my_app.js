610f11b574d7932f7f818cba248dc987abbd491c
Florencia Lema <flor.lema222@gmail.com>
2022-06-03 18:09:07 -0300

    test 2

--- app/javascript/packs/my_app.js
+++ app/javascript/packs/my_app.js
@@ -11,3 +11,10 @@ function test () {
 }
 
 test()
+
+function test2 () {
+  const sentence = prompt("Write something as a tweet: ")
+  alert("You've written " + sentence.length + " characters, you have " + (240 -(sentence.length)) + " characters left") ;
+}
+
+test2()
