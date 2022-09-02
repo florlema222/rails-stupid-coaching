67680b69e9f75974226d241aa20b60f6ff5e0773
Florencia Lema <flor.lema222@gmail.com>
2022-09-02 17:48:56 -0300

    all question tests done/passed

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -4,6 +4,7 @@ class QuestionsTest < ApplicationSystemTestCase
   test "visiting /ask renders the form" do
     visit ask_url
     assert_selector "label", text: "Ask your coach anything"
+    take_screenshot
   end
 
   test "saying Hello yields a grumpy response from the coach" do
@@ -12,5 +13,27 @@ class QuestionsTest < ApplicationSystemTestCase
     click_on "Ask!"
 
     assert_text "I don't care, get dressed and go to work!"
+    take_screenshot
+  end
+
+  test "asking sth to coach gets silly question response from coach" do
+    visit ask_url
+    fill_in "question", with: "what's up?"
+    click_on "Ask!"
+    assert_text "Silly question, get dressed and go to work!"
+  end
+
+  test "writting in uppercase the right response gives the feel motivation response" do
+    visit ask_url
+    fill_in "question", with: "I AM GOING TO WORK RIGHT NOW!"
+    click_on "Ask!"
+    assert_text "I can feel your motivation!"
+  end
+
+  test "writting the right response gives great! response" do
+    visit ask_url
+    fill_in "question", with: "I am going to work right now!"
+    click_on "Ask!"
+    assert_text "Great!"
   end
 end
